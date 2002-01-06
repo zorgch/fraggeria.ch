8344bbcbc59be500c239ee553e80d33d2c0f52f0
Cylander <cylander@zooomclan.org>
2002-01-06 22:12:06 +0100

    Ihr könnt euch nun für die zooom LAN anmelden

---
+++ index.php
@@ -0,0 +1,364 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
+
+  <html>
+
+  <head>
+
+  <title>Fraggeria! the zooom LAN</title>
+
+  <meta content="de" http-equiv="content-language"/>
+
+  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
+
+  <meta http-equiv="content-encoding" content="gzip"/>
+
+  <meta name="revisit-after" content="20 days"/>
+
+  <style type="text/css">
+
+  <!--
+
+  body {
+
+    background-color: #000000;
+
+    color: #FFFFFF;
+
+    font-size: 12px;
+
+  }
+
+  a:hover {
+
+    background-color: #007070;
+
+    text-decoration: none;
+
+    color: #FFFFFF;
+
+  }
+
+  a:link{
+
+    color: #FFFFFF;
+
+  }
+
+  table.main {
+
+    font-family: Verdana;
+
+    font-size: 12px;
+
+    vertical-align: top;
+
+    border-width: 0px;
+
+  }
+
+  b{
+
+    font-weight: 600;
+
+  }
+
+  a:active{
+
+    color: #FFFFFF;
+
+    text-decoration: none;
+
+  }
+
+  a:visited {
+
+    color: #FFFFFF;
+
+    text-decoration: underline;
+
+  }
+
+  big{
+
+    font-weight: bold;
+
+    font-size: 15px;
+
+  }
+
+  td{
+
+    vertical-align: top;
+
+    font-size: 10px;
+
+  }
+
+  td.main {
+
+    background-color: #005555;
+
+    border-style: solid;
+
+    border-width: 1px;
+
+    border-color: #00AAAA;
+
+  }
+
+  .title {
+
+    font-weight: bold;
+
+    border-style: solid;
+
+    border-color: #00CCCC;
+
+    border-width: 1px;
+
+    background-color: #004040;
+
+    font-size: 12px;
+
+  }
+
+  small{
+
+    font-size: 10px;
+
+  }
+
+  .normal{
+
+    font-size: 12px;
+
+  }
+
+  img{
+
+    border: 0px;
+
+    background-color: #005555;
+
+  }
+
+  .submit{
+
+    font-family: verdana;
+
+    font-size: 11px;
+
+    font-weight: 600;
+
+    border-style: outset;
+
+    border-width: 1px;
+
+    border-color: #00CCCC;
+
+    background-color: #004040;
+
+    color: #FFFFFF;
+
+  }
+
+  .text{
+
+    font-family: verdana;
+
+    font-size: 11px;
+
+    border-style: inset;
+
+    border-width: 1px;
+
+    border-color: #00CCCC;
+
+    background-color: #004040;
+
+    color: #FFFFFF;
+
+  }
+
+  .border{
+
+    border-style: solid;
+
+    border-color: #00CCCC;
+
+    border-width: 1px;
+
+  }
+
+	select{
+
+		font-family: verdana;
+
+		font-size: 11px;
+
+		background-color: #004040;
+
+		color: #FFFFFF;
+
+	}
+
+  -->
+
+  </style>
+
+  </head>
+
+  <body style="font-size: 12px;">
+  <table width="100%">
+
+  <tr>
+
+  <td align="center">
+
+  <table cellpadding="5" cellspacing="10" width="800" class="main">
+
+  <tr>
+
+  <td class="main" colspan="2" style="width: 800px;">
+
+  
+  <table width="100%" cellpadding="0" cellspacing="0">
+  <tr>
+  <td>
+  
+  <big>
+
+  <img src="img/logo/pink.png" alt="fraggeria logo"/>
+  </big>
+
+
+  </td>
+  <td align="right" style="width: 200px;"><form action="index.php" method="post"><input type="hidden" name="sessid" value="03a0aaf5a2147a00256e7185e9771140"/>
+<table><tr><td>
+<input type="text" name="loginuser" class="text" size="10" tabindex="1"/>
+</td><td>
+<small>Name</small>
+</td><td rowspan="2">
+<input type="submit" name="loginnow" value="login" class="submit" tabindex="3"/>
+</td></tr><tr><td>
+<input type="password" name="loginpw" class="text" size="10" tabindex="2"/>
+</td><td>
+<small>Passwort</small>
+</td></tr></table></form>
+</td></tr></table>
+  </td>
+
+  </tr>
+
+  <tr>
+
+  <td class="main" style="width: 120px;">
+
+  <table width="120">
+
+  <tr>
+
+  <td class="title">
+
+  Menu
+
+  </td>
+
+  </tr>
+
+  <tr>
+
+  <td align="left">
+<a href="index.php">News</a>
+<br/><a href="user.php">Anmelden</a>
+<a href="info.php">Information</a>
+<br/><a href="team.php">Team</a>
+<br/><a href="rules.php">Rules</a>
+<br/><a href="teilnehmer.php">Teilnehmer</a>
+<br/><a href="sitzplan.php">Sitzplan</a>
+<br/><a href="location.php">Location</a>
+<br/><a href="forum.php">Forum</a>
+</td>
+
+  </tr>
+
+  </table>
+
+  </td>
+
+  <td class="main" rowspan="2" style="width: 660px;">
+<table>
+<tr>
+<td class="title" style="width: 660px;">
+News 26.01.2002</td>
+</tr>
+</table>
+<br/>
+<table><tr><td class="title" style="width: 660px;" align="left">Page eröffnung by [z]cylander on 06. Jan. - 22:07</td></tr><tr><td align="left"><br/>So, die Page ist nun offiziell online. Anmeldung, Sitzplan reservierung, Forum und alles sollte funktionieren.
+<br/>
+
+<br/>
+Bei fehler bitte an info@fraggeria.ch mailen. 
+<br/>
+
+<br/>
+Danke<br/></td></tr><tr><td style="text-align: right;" align="left"><a href="index.php?nid=1">comments (...0)</a>
+</td></tr></table><br/><br/></td></tr><tr>
+
+  <td class="main" style="width: 120px">
+
+  
+  <table width="120">
+
+  <tr>
+
+  <td class="title">
+
+  Validation
+  </td>
+
+  </tr>
+
+  <tr>
+
+  <td style="text-align: center;">
+
+  <br/>
+  <a href="http://jigsaw.w3.org/css-validator/validator?uri=http://www.fraggeria.ch/index.php">
+    <img src="img/valid-css.gif" style="width:88px;height:31px" alt="Valid CSS!"/>
+
+  </a>
+  <br/>
+  <br/>
+  <a href="http://validator.w3.org/check/referer">
+
+    <img src="img/valid-xhtml.png" alt="Valid XHTML 1.0!" style="width:88px;height:31px"/>
+
+  </a>
+  </td>
+  </tr>
+  </table>
+
+  </td>
+
+  </tr><tr>
+
+  <td class="main" colspan="3" style="text-align: center; width: 800px;">
+
+  Copyright &copy; 2001 - 2002 <a href="http://www.zooomclan.org/">zooomclan</a>
+  </td>
+
+  </tr>
+
+  </table>
+
+  </td>
+
+  </tr>
+
+  </table>
+
+  </body>
+
+  </html>
