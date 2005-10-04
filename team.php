1e69a42adf15337636974e9730c2364d1da304fd
Cylander <cylander@zooomclan.org>
2005-10-04 18:33:23 +0200

    Fraggeria III - seid artig

--- team.php
+++ team.php
@@ -1,2 +1,202 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
+<html>
+<head>
 
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
+<title>Fraggeria III @zooomclan.org</title>
+<link rel="stylesheet" type="text/css" href="includes/day.css"><link rel="SHORTCUT ICON" href="https://zorg.ch/fav_day.ico" type="image/x-icon">
+<script language="javascript" type="text/javascript">
+function onoff(id) {
+	layer = document.getElementById("layer" + id)
+	image = "img" + id;
+  if(layer.style.visibility == "hidden") {
+    document.images[image].src = "/images/forum/day/minus.gif";
+    layer.style.display = "block";
+    layer.style.visibility = "visible";
+  } else {
+    document.images[image].src = "/images/forum/day/plus.gif";
+    layer.style.display = "none";
+    layer.style.visibility = "hidden";
+  }
+}
+
+function reply() {
+	location.hash = "reply";
+	document.commentform.text.focus();
+}
+
+function addsymbol(symbol) {
+	document.commentform.text.value = document.commentform.text.value + symbol;
+	document.commentform.text.focus();
+}
+</script>
+</head>
+
+<body>
+
+	<center>
+	
+		<table height="97%" bgcolor="#000000" cellspacing="0" cellpadding="0" width="860">
+		<tr><td valign="top" bgcolor="#F2F2F2" height="100%">
+				
+		
+<a name="top"></a>
+<div align="center">
+
+<table class="header" style="height: 55px; margin: 0px; width: 100%;">
+
+<tr>
+
+<td align="right" class="small" colspan="3" valign="middle" width="100%">
+
+</td>
+
+</tr>
+
+<tr>
+
+<td align="left">
+<a href="">
+<img alt="Zooomclan.org Logo" border="0" src="images/logo.png"/></a> 
+</td>
+
+<td align="center" style="link-color: red; font-size: x-small;" width="40%">
+
+<a href="smarty.php?tpl=158&amp;event_id=452">
+Toxic-Parteei
+ 03-04 Dec
+</a><br/>
+<a href="smarty.php?tpl=158&amp;event_id=450">
+battle of the bands halbfinal
+ 04. Dec 19Uhr
+</a><br/>
+<a href="smarty.php?tpl=158&amp;event_id=423">
+Apocalyptica Konzert
+ 04-05 Dec
+</a><br/>
+<a href="smarty.php?tpl=158&amp;event_id=448">
+Statuten-Bau-Event
+ 09. Dec 18Uhr
+</a><br/>
+
+</td>
+
+
+<td align="right">
+<table cellpadding="0" cellspacing="2">
+<tr>
+
+			<td align="right">
+			<table><tr>
+			<form action="smarty.php" method="post" name="loginform">
+			<td align="left" class="small">
+			<input tabindex="1" type="text" name="username" value="" class="text">
+			<input tabindex="2" type="password" name="password" class="text">
+			<input tabindex="3" type="submit" value="login" class="button">
+			<input type="checkbox" name="cookie"> autologin <br/>
+			
+			</td></tr>
+			</form></table>
+		  </td>
+			
+</tr>
+</table>
+
+
+</td>
+</tr>
+
+<tr><td align="left" colspan="3">
+
+<table cellspacing="0" cellpadding="0" width="100%"><tr>
+<td align="left" class="small">
+
+<b>Online Users:</b>
+<a href="profil.php?user_id=51" ">buezi</a>
+
+</td>
+<td class="small" width="300" align="right" valign="middle">
+<table cellpadding="0" cellspacing="0" border="0">
+<tr><td align="right">
+<img src="images/country/flags/USA.png" alt="united states" title="united states">
+&nbsp;
+</td><td class="small" align="right">
+Sunset: 17:04 </td>
+</tr>
+</table>
+</td>
+
+</tr>
+</table>
+</td></tr>
+
+</table>
+
+</div><div align="center" valign="top" style="margin: 0px 40px;">
+
+    <table width="100%" class="border"><tr><td width="100%">
+</div><div class="menu" align="center" width="100%"><a class="left">&nbsp;</a><a href="smarty.php?tpl=158">Events</a><a href="gallery.php">Gallery</a><a href="forum.php">Forum</a><a href="smarty.php?tpl=165">Games</a><a href="seti_xml.php">Space</a><a href="profil.php">User</a><a href="index.php">Home</a><a class="right">&nbsp;</a></div><div align="center" valign="top" style="margin: 0px 40px;">
+
+</div><div class="menu" align="center" width="100%"><a class="left">&nbsp;</a><a href="">no menu-items defined yet ya jou shit jeh den creh spirhendel je meh</a><a class="right">&nbsp;</a></div><div align="center" valign="top" style="margin: 0px 40px;">
+
+<img src="https://zorg.ch/files/10/f1-green.png" alt="F1 Logo">
+
+<h1>Fraggeria III - "seid artig"</h1>
+Yet Another Artgerechte Zooomclan LAN-Party<br/><br/>
+
+DATUM / ZEIT: Über Auffahrt 2006, ungefähr vom Samstag, 27. Mai 2006 10 Uhr bis Montag, 29. Mai 2006 23 Uhr<br/>
+ORT: Hilf mit, eine Location zu suchen, und gewinne ein Schützengarten Lager 0.33l!<br/><br/>
+
+<center><h2><blink><font color="violett">==&gt;&gt; <a href="http://www.zooomclan.org/smarty.php?tpl=158&amp;event_id=376">Zur Anmeldung</a> &lt;&lt;==</font></blink></h2></center>
+(halt den event joinen, hä)<br/><br/>
+
+<a href="http://www.zooomclan.org/forum.php?thread_id=59858&amp;parent_id=1#59858"><u><b>Das F3-Fundament</b></u></a><br/><br/>
+
+Mal schaun, ob's diesmal einen geben wird:<br/>
+<a href="http://www.zooomclan.org/forum.php?thread_id=64512&amp;parent_id=1#64512">Der F3-Trailer</a><br/><br/>
+
+<u>Organisation</u><br/>
+<a href="http://www.zooomclan.org/forum.php?thread_id=80581&amp;parent_id=1#80581">Kompetenzverteilung</a><br/><br/>
+
+<b>Freibier</b><br/>
+Cédi hat versprochen, 250.- für Freibier aufzuwerfen (wegen der <a href="http://www.zooomclan.org/smarty.php?tpl=158&amp;event_id=324&amp;parent_id=324#61596">Notebook-Geschichte</a>).
+Weiterhin fliesst Geld aus dem <a href="http://www.zooomclan.org/wetten.php">Wettbüro</a> (<a href="http://www.zooomclan.org/bugtracker.php?bug_id=520">Bug dazu</a>) in die Kasse.
+David verwaltet, so nett wie er ist, ein <a href="team.php" http: www.zooomclan.org forum.php?thread_id="59863&amp;parent_id=1#59863">Freibier-Konto</a>.<br/><br/>
+
+<a href="http://www.zooomclan.org/smarty.php?tpl=48">fraggeria I</a> - <a href="http://www.zooomclan.org/smarty.php?tpl=47">fraggeria II</a> - <a href="http://www.zooomclan.org/smarty.php?usershowcomments=1&amp;tpl=49&amp;parent_id=49">fraggeria III</a> - <a href="http://www.zooomclan.org/smarty.php?tpl=50">fraggeria IV</a> - fraggeria V
+
+<br/><br/>
+</p><table cellspacing="0" width="100%"><tr><td width="100%" class="small" align="right"><font color="green"><b>Kommentare</b> sind zur Zeit ausgeblendet. <a href="actions/show_tpl_comments.php?usershowcomments=1&amp;tpl=49team.php">Kommentare einblenden</a></font></td></tr></table></td></tr><tr><td bgcolor="#CCCCCC">
+
+<div align="right" class="small">
+
+
+
+<nobr>| updated: <a href="profil.php?user_id=10" ">Cedi</a>, 4. Oct 18:33</nobr>
+<nobr>| <a href="http://www.zooomclan.org/smarty.php?tpl=49">tpl=49</a></nobr>
+
+
+</div></td></tr></table>		
+
+
+<br/>
+      </div>
+      
+		</td></tr>
+      <tr>
+         <td width="100%" align="center" valign="center" class="small" bgcolor="#DDDDDD" style="padding: 2px; border-top-style: solid; border-top-width: 1px; border-top-color: #CCCCCC;">Parsetime: 
+            0.48s |
+            28 SQL Querys 
+      		 | <nobr>1.95 kB</nobr> | r: Alle | updated: Cedi, 4. Oct 18:33 | <a href="http://www.zooomclan.org/smarty.php?tpl=49">tpl=49</a> | word=f3<br/>
+            Magnetfeldrichtung: north  | Solarwind Dichte: 1.4 Protonen/cm<sup>3</sup> | Solarwind: 681.5 km/s | <a href="spaceweather.php">more &gt;&gt;</a><br/>
+            Samschtig 3. Dez&auml;mber Vi&auml;rtel ab Dr&uuml;&uuml; am Nomitag
+      	</td>
+      </tr>
+            
+     	</table>           
+      
+		
+      </center>
+		
+	</body>
+</html>
